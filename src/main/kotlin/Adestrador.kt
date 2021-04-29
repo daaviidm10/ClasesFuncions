f7eee788e9e23239a89d0d3ba827de6ecb7a2a70
David Mariño Alonso <dmarinoalonso@danielcastelao.org>
2021-04-29 18:13:21 +0200

    Initial commit

---
+++ src/main/kotlin/Adestrador.kt
@@ -0,0 +1,21 @@
+class Adestrador {
+
+    var id = 0
+    var nome = ""
+    var apelido = ""
+    var edade = 0
+    var idFederacion = 0
+
+    fun concentrarse(){
+
+    }
+
+    fun viaxar (pais:String){
+
+    }
+
+    fun dirixirPartido (){
+
+    }
+
+}
---
+++ src/main/kotlin/Xogador.kt
@@ -0,0 +1,24 @@
+class Xogador {
+
+    var id = 0
+    var nome = " "
+    var apelido = " "
+    var edade = 0
+    var dorsal = 0
+    var demarcación = " "
+
+    fun concentrarse():Unit{
+
+    }
+
+    fun viaxar(país:String){
+        println("alemania")
+    }
+    fun xogarPartido(){
+
+    }
+    fun entrenar(){
+
+    }
+
+}
